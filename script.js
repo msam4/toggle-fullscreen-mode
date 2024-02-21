dd747e21bb43fa5458638fe1746b494dd74a9b7c
Michael Sam <sam.michael.h@gmail.com>
2024-02-21 12:59:54 +0900

    Setup html css javascript file. Add basic html structure. Style the page from the tutorial. Add if statements for fullscreen and exiting with an addeventlistener to the button on click

---
+++ script.js
@@ -0,0 +1,30 @@
+let myDocument = document.documentElement;
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  if(btn.textContent == "Go Fullscreen") {
+    if(myDocument.requestFullscreen) {
+      myDocument.requestFullscreen();
+    } else if (myDocument.msRequestFullscreen) {
+      myDocument.msRequestFullscreen();
+    } else if (myDocument.mozRequestFullscreen) {
+      myDocument.mozRequestFullscreen();
+    } else if (myDocument.webkitRequestFullscreen) {
+      myDocument.webkitRequestFullscreen();
+    }
+
+    btn.textContent = "Exit Fullscreen"
+  } else {
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msexitFullscreen) {
+      document.msexitFullscreen();
+    } else if (document.mozexitFullscreen) {
+      document.mozexitFullscreen();
+    } else if (document.webkitexitFullscreen) {
+      document.webkitexitFullscreen();
+    }
+
+    btn.textContent = "Go Fullscreen";
+  }
+})
